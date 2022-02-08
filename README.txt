398180548998d6312834876f8988d792a0308d0b
mosakodi <mosakodi@gmail.com>
2022-02-08 10:21:55 -0800

    first commit

--- README.txt
+++ README.txt
@@ -14,12 +14,17 @@ once both are installed, on opens 'git bash'
 
 running p5:
 open terminal type git bash
-navigate to this project: cd Desktop/steep_and_fast
+navigate to this project: cd Desktop/moonphase
 run the server: http-server -c-1
 
 point browser to local host: http://localhost:8080/
 to stop server use ctrl C
 to install server: npm install -g http-server
+
+run the server 
+http-server -c-1
+
+point browser to local host: http://localhost:8080/
 
 while working on project:
 open terminal, go to project
--- sketch.js
+++ sketch.js
@@ -1,20 +1,27 @@
+let r = 87;
+let g = 9;
+let b = 222;
+
 function setup() {
-  createCanvas(600, 500);
-  fill(110);
+ createCanvas (800,800)
 }
 
 function draw() {
-  background(110);
-  strokeWeight(mouseX); 
-  print(mouseX);
+  newColor = color(87, 9, 222);
+  background(255,0,0)
+  shapeGroup(0, 0, 0, 255, 0);
+  shapeGroup(100, 100, r, g, b);
+  shapeGroup(200, 200, random(255), random(255), random(255));
+ } 
 
-  beginShape();
-  vertex(0, height);
-  vertex(width * 0.2, height * 0.4);
-  vertex(width * 0.35, height * 0.6);
-  vertex(width * 0.50, height * 0.2);
-  vertex(width * 0.70, height * 0.5);
-  vertex(width * 0.85, height * 0.3);
-  vertex(width, height);
-  endShape();
+function shapeGroup(x, y, red, green, blue){
+  fill(red, green, blue);
+  ellipse(x+200, y+200, 200, 200);
+  ellipse(x+400, y+400, 400, 400);
+  rect(x, y, 100, 100);
+  // print("shapeGroup is running")
+}
+
+function moon(phase ){
+
 }
